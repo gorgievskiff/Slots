eb7587b61564b657384b8c0d2c6b40a597cd764a
Ѓорѓиевски Филип <filip.gjorgjievski@students.finki.ukim.mk>
2021-06-01 19:15:31 +0200

    First commit

---
+++ slots/Form1.Designer.cs
@@ -0,0 +1,324 @@
+﻿
+namespace Slots
+{
+    partial class Slots
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.welcome = new System.Windows.Forms.Label();
+            this.row1col1 = new System.Windows.Forms.PictureBox();
+            this.btnSpin = new System.Windows.Forms.Button();
+            this.row1col2 = new System.Windows.Forms.PictureBox();
+            this.row1col3 = new System.Windows.Forms.PictureBox();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.row2col1 = new System.Windows.Forms.PictureBox();
+            this.row2col2 = new System.Windows.Forms.PictureBox();
+            this.row2col3 = new System.Windows.Forms.PictureBox();
+            this.row3col1 = new System.Windows.Forms.PictureBox();
+            this.row3col2 = new System.Windows.Forms.PictureBox();
+            this.row3col3 = new System.Windows.Forms.PictureBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnAddFunds = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbAvailableAmount = new System.Windows.Forms.TextBox();
+            this.cbCost = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblTotalWins = new System.Windows.Forms.Label();
+            this.tbTotal = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.row1col1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.row1col2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.row1col3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.row2col1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.row2col2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.row2col3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.row3col1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.row3col2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.row3col3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // welcome
+            // 
+            this.welcome.AutoSize = true;
+            this.welcome.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.welcome.Location = new System.Drawing.Point(43, 9);
+            this.welcome.Name = "welcome";
+            this.welcome.Size = new System.Drawing.Size(513, 69);
+            this.welcome.TabIndex = 1;
+            this.welcome.Text = "Welcome to Slots!";
+            // 
+            // row1col1
+            // 
+            this.row1col1.Location = new System.Drawing.Point(55, 95);
+            this.row1col1.Name = "row1col1";
+            this.row1col1.Size = new System.Drawing.Size(137, 104);
+            this.row1col1.TabIndex = 2;
+            this.row1col1.TabStop = false;
+            // 
+            // btnSpin
+            // 
+            this.btnSpin.Location = new System.Drawing.Point(911, 443);
+            this.btnSpin.Name = "btnSpin";
+            this.btnSpin.Size = new System.Drawing.Size(376, 86);
+            this.btnSpin.TabIndex = 3;
+            this.btnSpin.Text = "Spin";
+            this.btnSpin.UseVisualStyleBackColor = true;
+            this.btnSpin.Click += new System.EventHandler(this.btnSpin_Click);
+            this.btnSpin.MouseEnter += new System.EventHandler(this.btnSpin_MouseEnter);
+            this.btnSpin.MouseLeave += new System.EventHandler(this.btnSpin_MouseLeave);
+            // 
+            // row1col2
+            // 
+            this.row1col2.Location = new System.Drawing.Point(321, 95);
+            this.row1col2.Name = "row1col2";
+            this.row1col2.Size = new System.Drawing.Size(137, 104);
+            this.row1col2.TabIndex = 5;
+            this.row1col2.TabStop = false;
+            // 
+            // row1col3
+            // 
+            this.row1col3.Location = new System.Drawing.Point(585, 95);
+            this.row1col3.Name = "row1col3";
+            this.row1col3.Size = new System.Drawing.Size(137, 104);
+            this.row1col3.TabIndex = 6;
+            this.row1col3.TabStop = false;
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.Location = new System.Drawing.Point(911, 743);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(177, 53);
+            this.btnNewGame.TabIndex = 7;
+            this.btnNewGame.Text = "New Game";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // row2col1
+            // 
+            this.row2col1.Location = new System.Drawing.Point(55, 324);
+            this.row2col1.Name = "row2col1";
+            this.row2col1.Size = new System.Drawing.Size(137, 104);
+            this.row2col1.TabIndex = 8;
+            this.row2col1.TabStop = false;
+            // 
+            // row2col2
+            // 
+            this.row2col2.Location = new System.Drawing.Point(321, 324);
+            this.row2col2.Name = "row2col2";
+            this.row2col2.Size = new System.Drawing.Size(137, 104);
+            this.row2col2.TabIndex = 9;
+            this.row2col2.TabStop = false;
+            // 
+            // row2col3
+            // 
+            this.row2col3.Location = new System.Drawing.Point(585, 324);
+            this.row2col3.Name = "row2col3";
+            this.row2col3.Size = new System.Drawing.Size(137, 104);
+            this.row2col3.TabIndex = 10;
+            this.row2col3.TabStop = false;
+            // 
+            // row3col1
+            // 
+            this.row3col1.Location = new System.Drawing.Point(55, 561);
+            this.row3col1.Name = "row3col1";
+            this.row3col1.Size = new System.Drawing.Size(137, 104);
+            this.row3col1.TabIndex = 11;
+            this.row3col1.TabStop = false;
+            // 
+            // row3col2
+            // 
+            this.row3col2.Location = new System.Drawing.Point(321, 561);
+            this.row3col2.Name = "row3col2";
+            this.row3col2.Size = new System.Drawing.Size(137, 104);
+            this.row3col2.TabIndex = 12;
+            this.row3col2.TabStop = false;
+            // 
+            // row3col3
+            // 
+            this.row3col3.Location = new System.Drawing.Point(585, 561);
+            this.row3col3.Name = "row3col3";
+            this.row3col3.Size = new System.Drawing.Size(137, 104);
+            this.row3col3.TabIndex = 13;
+            this.row3col3.TabStop = false;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(1110, 743);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(177, 53);
+            this.btnExit.TabIndex = 14;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnAddFunds
+            // 
+            this.btnAddFunds.Location = new System.Drawing.Point(1067, 78);
+            this.btnAddFunds.Name = "btnAddFunds";
+            this.btnAddFunds.Size = new System.Drawing.Size(220, 53);
+            this.btnAddFunds.TabIndex = 15;
+            this.btnAddFunds.Text = "Add Funds";
+            this.btnAddFunds.UseVisualStyleBackColor = true;
+            this.btnAddFunds.Click += new System.EventHandler(this.btnAddFounds_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(905, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 36);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Wallet:";
+            // 
+            // tbAvailableAmount
+            // 
+            this.tbAvailableAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbAvailableAmount.Location = new System.Drawing.Point(1067, 31);
+            this.tbAvailableAmount.Name = "tbAvailableAmount";
+            this.tbAvailableAmount.ReadOnly = true;
+            this.tbAvailableAmount.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.tbAvailableAmount.Size = new System.Drawing.Size(220, 41);
+            this.tbAvailableAmount.TabIndex = 17;
+            // 
+            // cbCost
+            // 
+            this.cbCost.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbCost.FormattingEnabled = true;
+            this.cbCost.Location = new System.Drawing.Point(1067, 184);
+            this.cbCost.Name = "cbCost";
+            this.cbCost.Size = new System.Drawing.Size(141, 44);
+            this.cbCost.TabIndex = 18;
+            this.cbCost.SelectedValueChanged += new System.EventHandler(this.cbCost_SelectedValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(905, 192);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(147, 36);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Spin cost:";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(907, 561);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(181, 36);
+            this.lblResult.TabIndex = 20;
+            this.lblResult.Text = "Spin result...";
+            // 
+            // lblTotalWins
+            // 
+            this.lblTotalWins.AutoSize = true;
+            this.lblTotalWins.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalWins.Location = new System.Drawing.Point(905, 142);
+            this.lblTotalWins.Name = "lblTotalWins";
+            this.lblTotalWins.Size = new System.Drawing.Size(89, 36);
+            this.lblTotalWins.TabIndex = 21;
+            this.lblTotalWins.Text = "Total:";
+            // 
+            // tbTotal
+            // 
+            this.tbTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbTotal.Location = new System.Drawing.Point(1067, 137);
+            this.tbTotal.Name = "tbTotal";
+            this.tbTotal.ReadOnly = true;
+            this.tbTotal.Size = new System.Drawing.Size(220, 41);
+            this.tbTotal.TabIndex = 22;
+            // 
+            // Slots
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1299, 829);
+            this.Controls.Add(this.tbTotal);
+            this.Controls.Add(this.lblTotalWins);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbCost);
+            this.Controls.Add(this.tbAvailableAmount);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAddFunds);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.row3col3);
+            this.Controls.Add(this.row3col2);
+            this.Controls.Add(this.row3col1);
+            this.Controls.Add(this.row2col3);
+            this.Controls.Add(this.row2col2);
+            this.Controls.Add(this.row2col1);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.row1col3);
+            this.Controls.Add(this.row1col2);
+            this.Controls.Add(this.btnSpin);
+            this.Controls.Add(this.row1col1);
+            this.Controls.Add(this.welcome);
+            this.Name = "Slots";
+            this.Text = "Slots";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Slots_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.row1col1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.row1col2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.row1col3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.row2col1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.row2col2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.row2col3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.row3col1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.row3col2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.row3col3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label welcome;
+        private System.Windows.Forms.PictureBox row1col1;
+        private System.Windows.Forms.Button btnSpin;
+        private System.Windows.Forms.PictureBox row1col2;
+        private System.Windows.Forms.PictureBox row1col3;
+        private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.PictureBox row2col1;
+        private System.Windows.Forms.PictureBox row2col2;
+        private System.Windows.Forms.PictureBox row2col3;
+        private System.Windows.Forms.PictureBox row3col1;
+        private System.Windows.Forms.PictureBox row3col2;
+        private System.Windows.Forms.PictureBox row3col3;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnAddFunds;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbAvailableAmount;
+        private System.Windows.Forms.ComboBox cbCost;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblTotalWins;
+        private System.Windows.Forms.TextBox tbTotal;
+    }
+}
+
---
+++ slots/Slots/AddFounds.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Slots
+{
+    public partial class AddFunds : Form
+    {
+        public int Amount { get; set; }
+
+        public AddFunds()
+        {
+            InitializeComponent();
+            tbFunds.Focus();
+        }
+
+        private void tbFounds_Validating(object sender, CancelEventArgs e)
+        {
+            int newAmount;
+            if(String.IsNullOrEmpty(tbFunds.Text))
+            {
+                errorProvider1.SetError(tbFunds, "Amount required!");
+                e.Cancel = true;
+
+            }
+            else
+            {
+                if (!Int32.TryParse(tbFunds.Text, out newAmount))
+                {
+                    errorProvider1.SetError(tbFunds, "Only numbers are allowed!");
+                    e.Cancel = true;
+                }
+                else
+                {
+                    this.Amount = newAmount;
+                    errorProvider1.SetError(tbFunds, null);
+                    e.Cancel = false;
+                }
+            }
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+        }
+    }
+}
